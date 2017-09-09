d8c04cbca57f1f78c284f2f168c0575806637c97
Bill Paul <wpaul@windriver.com>
2017-09-09 14:17:34 -0700

    Document that nrf.h is a placeholder.

---
+++ sw/firmware/badge/nrf.h
@@ -0,0 +1,7 @@
+/*
+ * This is a dummy placeholder file. Some of the SoftDevice headers expect
+ * to include an nrf.h file from the Nordic SDK. We're not using this
+ * yet, but we need something for the files to #include to make things
+ * build, and I'd prefer to keep the original SoftDevice files intact
+ * instead of butchering them to match our setup.
+ */
