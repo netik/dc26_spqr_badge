ef5e181ee9c16ddf334e1b5e161e99df49180726
Bill Paul <wpaul@windriver.com>
2017-09-12 13:10:11 -0700

    Fix image entry point
    When debugging with gdb, we need the entry point address to point
    to the Reset_Vector address. But when using the SoftDevice object,
    that needs to be the SoftDevice reset vector, not the one from ChibiOS.
    Previously we were setting the entry point to 0. To make things
    work right, we need to manually set the softdeviceEntry symbol
    to the SoftDevice reset vector. This requires tweaking the mkelf.sh
    script a little bit.

--- sw/firmware/badge/softdevice/mkelf.sh
+++ sw/firmware/badge/softdevice/mkelf.sh
@@ -2,9 +2,13 @@
 
 # This script converts the softdevice.hex file into a softdevice.o ELF
 # object that we can link with ChibiOS to produce a complete OS image.
+# Note: the entry point address 0x8e9 is the address of the reset
+# handler in the SoftDevice module. This address was obtained by
+# doing a hex dump of the first few bytes of the object file.
 
 arm-none-eabi-objcopy -I ihex -O binary s132_nrf52_5.0.0_softdevice.hex softdevice.bin
 arm-none-eabi-objcopy -I binary -O elf32-littlearm -B arm softdevice.bin softdevice.o
+rm -f softdevice.bin
 arm-none-eabi-objcopy -I elf32-littlearm --rename-section .data=.softdevice softdevice.o
 arm-none-eabi-objcopy -I elf32-littlearm --set-section-flags .softdevice=CONTENTS,ALLOC,LOAD,CODE softdevice.o
-arm-none-eabi-objcopy -I elf32-littlearm --redefine-sym _binary_softdevice_bin_start=softdeviceEntry softdevice.o
+arm-none-eabi-objcopy -I elf32-littlearm --add-symbol softdeviceEntry=.softdevice:0x000008e9 softdevice.o
