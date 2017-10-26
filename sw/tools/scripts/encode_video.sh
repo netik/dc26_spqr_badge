5e43d5af80c4c6820f914d63a03b0a4d1b7ad98d
Bill Paul <wpaul@windriver.com>
2017-10-25 19:46:24 -0700

    More fixes for video encode and playback
    After doing some more calculations, I've settled on the following parameters:
    
    Video encode resulution: 160x120
    Video frame rate: 16 frames/second
    Audio sample rate: 11520Hz
    
    Video chunk size (max # of scanlines in a single SD card read): 16
    Audio samples per scanline: 6
    Audio samples per chunk: 96
    
    This is for monaural audio. If we try for stereo then the number of audio
    samples per scanline increases to 12 and samples per chunk becomes 192.
    
    These values are choosen to allow for relatively fast playback (larger SD
    card reads are faster) without consuming excessive amounts of RAM. (Yes we
    have more RAM this time around, but we should still be frugal with it.)

--- sw/tools/scripts/encode_video.sh
+++ sw/tools/scripts/encode_video.sh
@@ -9,7 +9,7 @@
 # Script to convert a video file into the format used by the
 # the video player on the Ides of DEF CON badge. The format isn't
 # very complicated: a .VID file contains raw RGB565 pixel data
-# and raw 12-bit audio sample data. The player just loads the data
+# and raw 16-bit audio sample data. The player just loads the data
 # and transfers it to the screen and DAC as fast as it can.
 #
 # Usage is:
@@ -21,9 +21,9 @@
 #
 # The destination directory must already exist
 #
-# The video will have an absolute resolution of 128x96 pixels and
-# a frame rate of 8 frames per second. These values have been chosen
-# to coincide with the audio sample rate of 9216Hz. The video
+# The video will have an absolute resolution of 160x120 pixels and
+# a frame rate of 16 frames per second. These values have been chosen
+# to coincide with the audio sample rate of 11520Hz. The video
 # and audio will be synchronized during playback.
 #
 
@@ -33,12 +33,12 @@ rm -f $2/video.bin
 ffmpeg -i "$1" -r 16 -s 160x120 -f rawvideo -pix_fmt rgb565 $2/video.bin
 
 # Extract audio
-ffmpeg -i "$1" -ac 1 -ar 12000 $2/sample.wav
+ffmpeg -i "$1" -ac 1 -ar 11520 $2/sample.wav
 
 # Convert WAV to to raw unsigned 16 bit samples, boost gain a little
-sox $2/sample.wav $2/sample.raw contrast 80
+sox $2/sample.wav $2/sample.u16
 
 # Now merge the video and audio into a single file
-./bin/videomerge $2/video.bin $2/sample.raw $2/video.vid
+./bin/videomerge $2/video.bin $2/sample.u16 $2/video.vid
 
-# rm -f $2/video.bin $2/sample.wav $2/sample.raw
+rm -f $2/video.bin $2/sample.wav $2/sample.u16 $2/sample.raw
--- sw/tools/src/videomerge.c
+++ sw/tools/src/videomerge.c
@@ -1,25 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <string.h>
 
 /*
  * This program merges a video and audio stream together into a single file
  * for playback on the Nordic NRF52 using our video player. The video stream
- * is a file containing sequential 128x96 resolution frames in 16-bit RGB565
+ * is a file containing sequential 160x120 resolution frames in 16-bit RGB565
  * pixel format. The audio stream is a file containing a stream of 16-bit
- * audio samples encoded at a rate of 12000Hz, stored 16 bits per sample.
+ * audio samples encoded at a rate of 11520Hz, stored 16 bits per sample.
  */
 
-#define LINES_PER_CHUNK		16
-#define SAMPLE_RATE		12000
+#define VID_FRAMES_PER_SEC		16
 
-#define FRAME_WIDTH		160
-#define FRAME_HEIGHT		120
+#define VID_PIXELS_PER_LINE		160
+#define VID_LINES_PER_FRAME		120
 
-#define SAMPLES_PER_CHUNK	100
+#define VID_CHUNK_LINES			16
 
-#define VID_BUFSZ		(FRAME_WIDTH * LINES_PER_CHUNK)
-#define AUD_BUFSZ		(SAMPLES_PER_CHUNK)
+#define VID_AUDIO_SAMPLES_PER_SCANLINE	6
+
+#define VID_AUDIO_CHANNELS		1
+
+#define VID_AUDIO_SAMPLE_RATE		11520
+
+#define VID_AUDIO_SAMPLES_PER_CHUNK		\
+	(VID_AUDIO_SAMPLES_PER_SCANLINE * VID_CHUNK_LINES)
+
+#define VID_AUDIO_BYTES_PER_CHUNK		\
+	(VID_AUDIO_SAMPLES_PER_CHUNK * 2)
+
+#define VID_BUFSZ			(VID_PIXELS_PER_LINE * VID_CHUNK_LINES)
+#define AUD_BUFSZ			(VID_AUDIO_SAMPLES_PER_CHUNK)
 
 int
 main (int argc, char * argv[])
@@ -29,10 +41,6 @@ main (int argc, char * argv[])
 	FILE * out;
 	uint16_t scanline[VID_BUFSZ];
 	uint16_t samples[AUD_BUFSZ];
-	int i;
-
-printf ("VIDBUF: %d\n", sizeof(scanline));
-printf ("AUDBUF: %d\n", sizeof(samples));
 
 	if (argc > 4)
 		exit (1);
@@ -61,22 +69,16 @@ printf ("AUDBUF: %d\n", sizeof(samples));
 		exit (1);
 	}
 
-	i = 0;
 	while (1) {
+		memset (samples, 0, sizeof(samples));
 		if (fread (scanline, sizeof(scanline), 1, video) == 0) {
-			printf ("OUT OF VIDEO!!\n");
 			break;
 		}
-		if (fread (samples, sizeof(samples), 1, audio) == 0) {
-			printf ("OUT OF AUDIO!!\n");
-			break;
-		}
-		i++;
-		fwrite (samples, sizeof(samples), 1, out);
+		(void) fread (samples, sizeof(samples), 1, audio);
 		fwrite (scanline, sizeof(scanline), 1, out);
+		fwrite (samples, sizeof(samples), 1, out);
 	}
 
-	printf ("%d PASSES\n", i);
 	fclose (video);
 	fclose (audio);
 	fclose (out);
