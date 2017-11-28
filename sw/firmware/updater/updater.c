e655353def0136d2863660ca172aa947896abfef
Bill Paul <wpaul@windriver.com>
2017-11-28 14:31:44 -0800

    Improve stubbing for some ChibiOS routines
    Rather than explicitly create stubs for several ChibiOS routines,
    we now just create one and use __attrribute__ to alias it to the
    remaining symbols. A few additional symbols have been added so that
    the code will build when LTO is disabled. (This makes debugging easier.)

--- sw/firmware/updater/updater.c
+++ sw/firmware/updater/updater.c
@@ -1,5 +1,5 @@
 /*-
- * Copyright (c) 2016
+ * Copyright (c) 2016-2017
  *      Bill Paul <wpaul@windriver.com>.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -121,16 +121,31 @@ mmc_callback(GPTDriver *gptp)
 	return;
 }
 
+/*
+ * We're using some ChibiOS drivers in the updater, but not using ChibiOS
+ * itself. We're careful to use APIs that don't require the ChibiOS kernel
+ * or scheduler, however in a couple of cases we still have some unresolved
+ * linker references. We need to stub these routines out. To save space,
+ * we can create one routine as a stub and alias all the other functions
+ * to it.
+ *
+ * Note that a few of these only show up when we build without LTO.
+ */
+
+__attribute__ ((alias ("_port_irq_epilogue")))
+void chMtxObjectInit (mutex_t *mp);
+__attribute__ ((alias ("_port_irq_epilogue")))
+void chThdResumeI (thread_reference_t *trp, msg_t msg);
+__attribute__ ((alias ("_port_irq_epilogue")))
+msg_t chThdSuspendS (thread_reference_t *trp);
+__attribute__ ((alias ("_port_irq_epilogue")))
+void chMtxLock (mutex_t *mp);
+__attribute__ ((alias ("_port_irq_epilogue")))
+void chMtxUnlock (mutex_t *mp);
+
 void
 _port_irq_epilogue (void)
 {
-	return;
-}
-
-void
-chMtxObjectInit (mutex_t *mp)
-{
-	(void)mp;
 	return;
 }
 
